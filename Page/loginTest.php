a255fa6db07da48c173195cd79b898bb2e5d1453
VickChelo <31808216+vickchelo16@users.noreply.github.com>
2017-10-08 20:49:43 -0700

    Pruebas Login Test

--- Page/loginTest.php
+++ Page/loginTest.php
@@ -1,31 +1,26 @@
 <?php
-
+    echo "<script>
+        function Click(){
+            alert('Click');
+        }
+    </script>";
 ?>
 
 <html>
 <title>Login test</title>
 <head>
     <link href="css/loginTest.css" rel="stylesheet"> 
-    <script>
-        function Click(){
-            alert('Click');
-        }
-    </script>
+    
 </head>
 <body>
     <p>Test</p>
-    <div id="idForm">
-    <form class="formLogin" action="loginTest.php" >
-        <input type="text" placeholder="Usuario" id="usuario">
-        <input type="text" placeholder="Contrasena" id="password">
-        <input type="submit" placeholder="Aceptar" id="btnAceptar" onclick="Click()">
-    </form>
-    <form class="formAdmin" action="loginTest.php">
-        <p>SOLO ADMINISTRACION</p>
-        <input type="text" placeholder="Usuario" id="usuario">
-        <input type="text" placeholder="Contrasena" id="password">
-        <input type="submit" placeholder="Aceptar" id="btnAceptar" onclick="Click()">
-    </form>
+    <div id="idForm"> 
+        <form class="formAdmin" action="loginTest.php">
+            <h1>Empleados </h1> 
+            <input type="text" placeholder="Usuario" id="usuario">
+            <input type="text" placeholder="Numero de Empleado" id="password">
+            <input type="submit" placeholder="Aceptar" id="btnAceptar" onclick="Click()">
+        </form>
     </div>
 </body>
-</html>
+</html>s
